bab60dea382d74a753bc080f94b19e514fe80746
kvasena <kvasena@gmail.com>
2016-03-09 18:09:53 +0200

    test8 for PythonAnywhere

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Post
+from .models import *
 
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('updated')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
