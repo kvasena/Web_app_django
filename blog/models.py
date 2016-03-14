473de7c14d3307f307ffcc55760cf5bb8c21f409
kvasena <kvasena@gmail.com>
2016-03-14 18:50:10 +0200

    Some new changes with static files to fix favicon.io 2

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from django.utils import timezone
+from datetime import datetime
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
-    updated = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=datetime.now())
+    updated = models.DateTimeField(default=datetime.now())
 
     def publish(self):
         self.save()
