ac3a52f96b9323e46e4c6099eec91b2d688331b8
kvasena <kvasena@gmail.com>
2016-03-14 17:02:04 +0200

    Some new changes with static files to fix favicon.io

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,4 @@ def post_list(request):
     posts = Post.objects.order_by('updated')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
+                                                                
